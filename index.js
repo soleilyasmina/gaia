817e90b424918131ca87a3276825cf05769f9d76
soleilyasmina <mounlasolomon@gmail.com>
2019-10-18 15:32:26 -0400

    added enrollments

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 // scope for readonly
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
+// where the token is saved
 const TOKEN_PATH = 'token.json';
 
+// acquiring a new token
 const getNewToken = (oAuth2Client, callback) => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -20,7 +22,7 @@ const getNewToken = (oAuth2Client, callback) => {
     input: process.stdin,
     output: process.stdout,
   });
-  // Node side readline for info
+  // Node-side readline for info
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
@@ -71,12 +73,17 @@ const sendMailTo = (email, text) => {
 };
 
 const listHomework = async (auth) => {
+  // it's time for us to grab some data
   const sheets = google.sheets({ version: 'v4', auth });
-  // grab lots of data
+  // grab the individual students names
   const studentsData = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SPREADSHEETID,
     range: 'Homework Completion!B6:E20',
   });
+  const enrollmentData = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEETID,
+    range: 'Course Roster and Progress!F5:F19'
+  })
   const assignmentsData = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SPREADSHEETID,
     range: 'Homework Completion!F5:BC5',
@@ -95,23 +102,21 @@ const listHomework = async (auth) => {
   const assignments = assignmentsData.data.values[0].filter((item) => !item.match(/[HW] [\d]/));
   const submissions = submissionsData.data.values;
   const attendances = attendancesData.data.values;
+  const enrollments = enrollmentData.data.values;
 
   // stringify homework
-  const assignHomework = studentSubmissions => assignments.map((item, index) => `${item}: ${studentSubmissions[index]}`);
+  const assignHomework = studentSubmissions => assignments.map((item, index) => ({ [item]: studentSubmissions[index] }));
 
   // preparing the final output
   const assignedStudents = students.map((item, index) => ({
     name: `${item[0]} ${item[1]}`,
     email: item[2],
-    submissions: assignHomework(submissions[index]),
     percentage: item[3],
     absences: attendances[index][0],
+    enrollment: enrollments[index][0],
+    submissions: assignHomework(submissions[index]),
   }));
-
-  if (process.argv[2] === 'email') {
-    assignedStudents.forEach(student => sendMailTo(student.email, JSON.stringify(student)));
-  }
-
+  
   console.table(assignedStudents);
   return assignedStudents;
 };
