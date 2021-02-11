766f41bfeaba0d98ae7e862ccfe8e693b76d7bc4
soleilyasmina <mounlasolomon@gmail.com>
2021-02-10 22:49:55 -0500

    fix wiki gen

--- wiki/index.js
+++ wiki/index.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
-const axios = require("axios");
 const fs = require("fs");
 const { prompt } = require("inquirer");
+const table = require("markdown-table");
 
 const inquire = async () => {
   return prompt([
@@ -21,7 +21,7 @@ const inquire = async () => {
 
 const buildLessons = async (auth, { unit, spreadsheetId }) => {
   try {
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
     const lessonsData = await sheets.spreadsheets.get({
       spreadsheetId,
       ranges: [`${unit}!A1:K140`],
@@ -30,13 +30,14 @@ const buildLessons = async (auth, { unit, spreadsheetId }) => {
     console.log(lessonsData.data.sheets[0].data[0].rowData);
     const lessons = lessonsData.data.sheets[0].data[0].rowData
       .map(({ values }) => values)
-      .filter((val) => val[1] !== '')
+      .filter((val) => val[1] !== "")
       .map((line) => ({
         date: line[0].formattedValue,
         type: line[1].formattedValue,
         name: line[7].formattedValue,
         link: line[7].hyperlink,
-        solution: line[8].hyperlink,
+        solution: line[8].formattedValue,
+        solutionLink: line[8].hyperlink,
         zoom: line[9].hyperlink,
       }));
     return lessons;
@@ -46,33 +47,54 @@ const buildLessons = async (auth, { unit, spreadsheetId }) => {
 };
 
 const buildDays = (lessons) => {
-  console.log(lessons);
-  const [actualDays] = lessons.reduce((acc, curr) => {
-    if (acc[1]) {
-      acc[0].push(curr);
-    } else {
-      acc[1] = curr.date === undefined || curr.date.includes('UNIT');
-    }
-    return acc;
-  }, [[], false]);
-  const separatedDays = actualDays.reduce((acc, curr) => {
-    if (curr.type === undefined) {
-      acc.push([]);
-    } else {
-      acc[acc.length - 1].push(curr);
-    }
-    return acc;
-  }, [[]]);
-
-  return separatedDays.filter((day) => day.length > 0);
-}
+  const [actualDays] = lessons.reduce(
+    (acc, curr) => {
+      if (acc[1]) {
+        acc[0].push(curr);
+      } else {
+        acc[1] = curr.date && curr.date.includes("UNIT");
+      }
+      return acc;
+    },
+    [[], false]
+  );
+  const separatedDays = actualDays.reduce(
+    (acc, curr) => {
+      if (curr.type === undefined) {
+        acc.push([]);
+      } else {
+        acc[acc.length - 1].push(curr);
+      }
+      return acc;
+    },
+    [[]]
+  );
+  const realDays = separatedDays.filter((day) => day.length > 0);
+  console.log(realDays);
+  const tableTime = table([
+    ["Date", "Type", "Repo", "Solution", "Recording"],
+    ...realDays.flat().map((line) => [
+        line.date,
+        line.type,
+        line.link ? `[${line.name}](${line.link})` : line.name ? line.name : "",
+      line.solutionLink ? `[${line.solution}](${line.solutionLink})` : line.solution ? `[${line.solution}](${line.link}/tree/solution)` : "",
+        line.zoom,
+      ]),
+  ]);
+  return tableTime;
+};
 
 const createWiki = async (auth) => {
   const sheets = google.sheets({ version: "v4", auth });
   const results = await inquire();
   const lessons = await buildLessons(auth, results);
   const days = buildDays(lessons);
-  days.forEach((day) => console.table(day))
+  fs.writeFileSync(
+    `./wiki/${process.env.COHORT}-${results.unit
+      .replace(/\ /g, "-")
+      .toLowerCase()}-wiki.md`,
+    days
+  );
 };
 
 module.exports = createWiki;
